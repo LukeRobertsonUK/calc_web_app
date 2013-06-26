3fc5546a9a1a4b4e3c7051017bb452e9dc4ddd7b
Luke Robertson <luke.robertson@mac.com>
2013-06-26 15:14:38 +0100

    adds calculator form

--- hi.rb
+++ hi.rb
@@ -3,28 +3,42 @@ require 'sinatra'
 require 'sinatra/contrib/all'
 
 
-get '/hi' do
-  "Hello, WDI"
-end
+# get '/hi' do
+#   "Hello, WDI"
+# end
 
-get '/' do
-  "This is the homepage"
-end
+# get '/' do
+#   "This is the homepage"
+# end
 
-get '/name/:first' do
-  "Hello, #{params[:first]}"
-end
+# get '/name/:first' do
+#   "Hello, #{params[:first]}"
+# end
 
-get '/name/:first/:last/:age' do
-  "your name is #{params[:first]} #{params[:last]}. you are #{params[:age]} years old."
-end
+# get '/name/:first/:last/:age' do
+#  erb :name
+# end
 
-get '/multiply/:x/:y' do
-  @result = params[:x].to_f * params[:y].to_f
-  erb :calc
-end
+# get '/multiply/:x/:y' do
+#   @result = params[:x].to_f * params[:y].to_f
+#   erb :calc
+# end
 
-get '/divide/:x/:y' do
-  @result = params[:x].to_f / params[:y].to_f
+# get '/divide/:x/:y' do
+#   @result = params[:x].to_f / params[:y].to_f
+#   erb :calc
+# end
+
+get '/calc' do
+  @first = params[:first].to_f
+  @second = params[:second].to_f
+  @operator = params[:operator]
+
+  @result = case @operator
+    when '+' then @first + @second
+    when '-' then @first - @second
+    when '*' then @first * @second
+    when '/' then @first / @second
+  end
   erb :calc
 end
